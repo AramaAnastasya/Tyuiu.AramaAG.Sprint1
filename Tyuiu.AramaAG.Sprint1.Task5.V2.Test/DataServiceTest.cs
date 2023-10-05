80505ff10b904e8dff92f0159d4f8cdb3057bdfa
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-10-05 18:08:24 +0500

    Task5

---
+++ Tyuiu.AramaAG.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AramaAG.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.AramaAG.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 59.5;
+            double res = ds.FahrenheitToСelsius(x);
+            Assert.AreEqual(15, res);
+        }
+    }
+}
---
+++ Tyuiu.AramaAG.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AramaAG.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.AramaAG.Sprint1.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Арама А.Г. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Арама Анастасия Георгиевна | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат и печатает результат на экране.                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double temp;
+            Console.WriteLine("* Введите значение температуры в градусах Фаренгейта:");
+            temp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.FahrenheitToСelsius(temp));
+            Console.ReadKey();
+        }
+    }
+}
